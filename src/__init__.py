b9e406363b3a680c9ffcb8b5123bbfe269164bcc
Emmanuel Adutwum <123039301+emmanueladutwum123@users.noreply.github.com>
2026-01-21 04:02:20 -0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,8 @@
 """
-Data Collection Package
+Soccer Prediction Master - Main Package
 """
 
-from .main import DataCollector
-
 __version__ = "1.0.0"
-__all__ = ['DataCollector']
+__author__ = "Soccer Prediction Team"
+
+print(f"⚽ Soccer Prediction Master v{__version__}")
